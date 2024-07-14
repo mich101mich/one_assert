08e4f4bb251616a2553b4eb9fc1f85b865a3bc93
mich101mich <mich101mich@gmail.com>
2024-07-14 18:24:30 +0200

    Removed test cases that differed between local and CI

--- tests/fail/expr/nightly/methodcall.rs
+++ tests/fail/expr/nightly/methodcall.rs
@@ -1,5 +1,4 @@
 fn main() {
     one_assert::assert!("".to_string().len());
-    one_assert::assert!("".len(1));
     one_assert::assert!(1.len());
 }
--- tests/fail/expr/path.rs
+++ tests/fail/expr/path.rs
@@ -1,5 +1,4 @@
 fn main() {
     one_assert::assert!(std::vec);
-    one_assert::assert!(std::vec::Vec);
     one_assert::assert!(std::f32::consts::PI);
 }
